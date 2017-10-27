6896a7d08a26584282a4e1c9ee5507bc399722f9
The Kyle Yap <famasboy88@gmail.com>
2017-10-28 00:22:37 +0800

    Linked most of the Activities but error in Google Signin API

--- app/src/main/java/com/zywie/android/zywie/LandingPage.java
+++ app/src/main/java/com/zywie/android/zywie/LandingPage.java
@@ -3,11 +3,11 @@ package com.zywie.android.zywie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class homeSignIn extends AppCompatActivity {
+public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_sign_in);
+        setContentView(R.layout.activity_landing_page);
     }
 }
