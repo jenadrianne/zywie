cba06b2a4e5c75a42e685970f5bf79682744876f
criscarlotan <criscarlo_tan@yahoo.com>
2017-10-29 10:49:30 +0800

    Update AccountActivity.java
    Working log-out button for google sign-in
    Disconnects current user account and can choose another account to log-in

--- app/src/main/java/com/zywie/android/zywie/AccountActivity.java
+++ app/src/main/java/com/zywie/android/zywie/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.zywie.android.zywie;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,11 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountActivity extends AppCompatActivity {
 
 
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +30,39 @@ public class AccountActivity extends AppCompatActivity {
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(AccountActivity.this, "Logout successful", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(AccountActivity.this, MainActivity.class));
+                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.clearDefaultAccountAndReconnect().setResultCallback(new ResultCallback<Status>() {
+
+                        @Override
+                        public void onResult(Status status) {
+
+                            mGoogleApiClient.disconnect();
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(AccountActivity.this, MainActivity.class));
+                            Toast.makeText(AccountActivity.this,"Logged out",Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
             }
         });
 
 
     }
 
-
+    @Override
+    protected void onStart() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
 
 }
+
